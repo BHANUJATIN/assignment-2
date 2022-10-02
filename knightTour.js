ac32c9c86757ee386cdc27c3e750489916170046
BhanuJatin <bjsbainsla@gmail.com>
2022-10-02 15:15:21 +0530

    some erros are removed

--- knightTour.js
+++ knightTour.js
@@ -1,31 +1,31 @@
-var knightpath=function(){};
+var knightTour=function(){};
 // internal variables
-knightpath.prototype.sizeX=450;//preferred width of the game board in px
-knightpath.prototype.sizeY=450;//preferred height of the game board in px
-knightpath.prototype.rankX=8;//rank of the board on the x-axis
-knightpath.prototype.rankY=8;//rank of the board on the y-axis
-knightpath.prototype.fieldArr=[];//the board itself
-knightpath.prototype.curId=0;//number of the last move made
-knightpath.prototype.allowedSquares=0;//number of squares allowed to move into after last move
-knightpath.prototype.lastPos=[];//coordinates of last move
-knightpath.prototype.wantsAutoMove=true;//use auto move?
-knightpath.prototype.figureMoves=[2,1];//movement shape of the piece used (knight - (2,1), cammel - (3,1), zebra - (3,2))
-// interface
-knightpath.prototype.gameBoardId="game-board";//id of div designated to be gameboard
-knightpath.prototype.gameBoardRowClass="row";//css class for gameboard rows
-knightpath.prototype.gameBoardColumnClass="col";//css class for gameboard rows
+knightTour.prototype.sizeX=450;//preferred width of the game board in px
+knightTour.prototype.sizeY=450;//preferred height of the game board in px
+knightTour.prototype.rankX=8;//rank of the board on the x-axis
+knightTour.prototype.rankY=8;//rank of the board on the y-axis
+knightTour.prototype.fieldArr=[];//the board itself
+knightTour.prototype.curId=0;//number of the last move made
+knightTour.prototype.allowedSquares=0;//number of squares allowed to move into after last move
+knightTour.prototype.lastPos=[];//coordinates of last move
+knightTour.prototype.wantsAutoMove=true;//use auto move?
+knightTour.prototype.figureMoves=[2,1];//movement shape of the piece used (knight - (2,1), cammel - (3,1), zebra - (3,2))
+// gameboard interface
+knightTour.prototype.gameBoardId="game-board";//id of div designated to be gameboard
+knightTour.prototype.gameBoardRowClass="row";//css class for gameboard rows
+knightTour.prototype.gameBoardColumnClass="col";//css class for gameboard rows
 // square styles interfaces
-knightpath.prototype.squareClass="cell";//css class for all squares
-knightpath.prototype.allowedSquareClass="allowed";//css class for allowed squares
-knightpath.prototype.autoMoveSquareClass="automoved";//css class for automoved
-knightpath.prototype.currentSquareClass="current";//css class for current square
+knightTour.prototype.squareClass="cell";//css class for all squares
+knightTour.prototype.allowedSquareClass="allowed";//css class for allowed squares
+knightTour.prototype.autoMoveSquareClass="automoved";//css class for automoved
+knightTour.prototype.currentSquareClass="current";//css class for current square
 // messaging interface
-knightpath.prototype.messageInterfaceId="log";//id of div containing messages
-knightpath.prototype.clearMessageInterface=function() {$("#"+this.messageInterfaceId).text("").css("display","none");}
-knightpath.prototype.prepareMessageInterface=function() {$("#"+this.messageInterfaceId).css("display","block");}
-knightpath.prototype.appendToMessageInterface=function(t) {$("#"+this.messageInterfaceId).append("<p>"+t+"</p>");}
+knightTour.prototype.messageInterfaceId="log";//id of div containing messages
+knightTour.prototype.clearMessageInterface=function() {$("#"+this.messageInterfaceId).text("").css("display","none");}
+knightTour.prototype.prepareMessageInterface=function() {$("#"+this.messageInterfaceId).css("display","block");}
+knightTour.prototype.appendToMessageInterface=function(t) {$("#"+this.messageInterfaceId).append("<p>"+t+"</p>");}
 // functions
-knightpath.prototype.initialize=function () {
+knightTour.prototype.initialize=function () {
 	this.clearMessageInterface();
 	this.curId=0;
 	this.allowedSquares=0;
@@ -52,22 +52,22 @@ knightpath.prototype.initialize=function () {
 	$("#"+this.gameBoardId+" ."+this.squareClass).css("width",d+"px").css("height",d+"px").css("font-size",Math.floor(d*this.fontFraction())+"px");
 	this.highLightSquares();
 }
-knightpath.prototype.fontFraction=function() {
+knightTour.prototype.fontFraction=function() {
 	var full=this.rankX*this.rankY;
 	if(full<10) return 0.75;
 	else if(full<100) return 0.6;
 	else return 0.45;
 }
-knightpath.prototype.isAllowed=function(x,y) {return this.isSquareVal(x,y,0);}
-knightpath.prototype.allowSquare=function(x,y) {
+knightTour.prototype.isAllowed=function(x,y) {return this.isSquareVal(x,y,0);}
+knightTour.prototype.allowSquare=function(x,y) {
 	this.setSquareValue(x,y,0);
 	this.allowedSquares++;
 }
-knightpath.prototype.isEmpty=function(x,y) {return this.isSquareVal(x,y,-1);}
-knightpath.prototype.emptySquare=function(x,y) {this.setSquareValue(x,y,-1);this.setSquareText(x,y,"");}
-knightpath.prototype.isSquareVal=function(x,y,v) {return (this.fieldArr[y][x]==v);}
-knightpath.prototype.setSquareValue=function(x,y,v) {this.fieldArr[y][x]=v;}
-knightpath.prototype.makeMoveTo=function(x,y) {
+knightTour.prototype.isEmpty=function(x,y) {return this.isSquareVal(x,y,-1);}
+knightTour.prototype.emptySquare=function(x,y) {this.setSquareValue(x,y,-1);this.setSquareText(x,y,"");}
+knightTour.prototype.isSquareVal=function(x,y,v) {return (this.fieldArr[y][x]==v);}
+knightTour.prototype.setSquareValue=function(x,y,v) {this.fieldArr[y][x]=v;}
+knightTour.prototype.makeMoveTo=function(x,y) {
 	this.curId++;
 	this.lastPos=[x,y];
 	this.setSquareValue(x,y,this.curId);
@@ -76,7 +76,7 @@ knightpath.prototype.makeMoveTo=function(x,y) {
 	this.addSquareClass(x,y,this.currentSquareClass);
 	this.applyMovePattern(x,y);
 }
-knightpath.prototype.undo=function() {
+knightTour.prototype.undo=function() {
 	this.clearMessageInterface();
 	if(this.curId==1) {
 		this.initialize();
@@ -98,7 +98,7 @@ knightpath.prototype.undo=function() {
 	this.highLightSquares();
 	this.wantsAutoMove=tmpAMove;
 }
-knightpath.prototype.obtainMovePattern=function(x,y) {
+knightTour.prototype.obtainMovePattern=function(x,y) {
 	var rez=[];
 	for(var dirs=0;dirs<8;dirs++) {
 		var trez=this.obtainSingleMove(x,y,dirs);
@@ -106,7 +106,7 @@ knightpath.prototype.obtainMovePattern=function(x,y) {
 	}
 	return rez;
 }
-knightpath.prototype.obtainSingleMove=function(x,y,d) {
+knightTour.prototype.obtainSingleMove=function(x,y,d) {
 	//right-most bit determines how many squares are moved
 	// on x and y coordinates
 	var moves=[this.figureMoves[0],this.figureMoves[1]];
@@ -127,7 +127,7 @@ knightpath.prototype.obtainSingleMove=function(x,y,d) {
 	//returns coordinates otherwise
 	return moves;
 }
-knightpath.prototype.applyMovePattern=function(x,y) {
+knightTour.prototype.applyMovePattern=function(x,y) {
 	// forbid all possible
 	for(var yi=0;yi<this.rankY;yi++) {
 		for(var xi=0;xi<this.rankX;xi++) {
@@ -145,7 +145,7 @@ knightpath.prototype.applyMovePattern=function(x,y) {
 	// if automove possible
 	else if(this.allowedSquares==1) this.autoMove();
 }
-knightpath.prototype.autoMove=function() {
+knightTour.prototype.autoMove=function() {
 	if(!this.wantsAutoMove) return ;
 	var moveMade=false;
 	for(var yi=0;yi<this.rankY && !moveMade;yi++) {
@@ -158,19 +158,19 @@ knightpath.prototype.autoMove=function() {
 		}
 	}
 }
-knightpath.prototype.addSquareClass=function(x,y,c) {
+knightTour.prototype.addSquareClass=function(x,y,c) {
 	$(this.getSquareId(x,y)).addClass(c);
 }
-knightpath.prototype.removeClass=function(c) {
+knightTour.prototype.removeClass=function(c) {
 	$("#"+this.gameBoardId+" ."+c).removeClass(c);
 }
-knightpath.prototype.setSquareText=function(x,y,t) {
+knightTour.prototype.setSquareText=function(x,y,t) {
 	$(this.getSquareId(x,y)).text(t);
 }
-knightpath.prototype.getSquareId=function(x,y) {
+knightTour.prototype.getSquareId=function(x,y) {
 	return "#c"+y+"x"+x;
 }
-knightpath.prototype.onSquareClick=function(id) {
+knightTour.prototype.onSquareClick=function(id) {
 	this.removeClass(this.autoMoveSquareClass);
 	var coords=id.substr(1);
 	coords=coords.split("x");
@@ -184,7 +184,7 @@ knightpath.prototype.onSquareClick=function(id) {
 	}
 	return false;
 }
-knightpath.prototype.highLightSquares=function() {
+knightTour.prototype.highLightSquares=function() {
 	this.removeClass(this.allowedSquareClass);
 	for(var yi=0;yi<this.rankY;yi++) {
 		for(var xi=0;xi<this.rankX;xi++) {
@@ -192,7 +192,7 @@ knightpath.prototype.highLightSquares=function() {
 		}
 	}
 }
-knightpath.prototype.isClosedTour=function() {
+knightTour.prototype.isClosedTour=function() {
 	if(this.curId==this.rankX*this.rankY) {
 		var x=this.lastPos[0];
 		var y=this.lastPos[1];
@@ -204,7 +204,7 @@ knightpath.prototype.isClosedTour=function() {
 	}
 	return false;
 }
-knightpath.prototype.gameOver=function() {
+knightTour.prototype.gameOver=function() {
 	this.prepareMessageInterface();
 	if(this.curId==this.rankX*this.rankY) this.appendToMessageInterface("Game over! You managed to fill whole "+this.rankX+"x"+this.rankY+" board.")
 	else this.appendToMessageInterface("Game over! You managed to visit "+this.curId+" squares (out of "+(this.rankX*this.rankY)+" possible) on "+this.rankX+"x"+this.rankY+" board.")
